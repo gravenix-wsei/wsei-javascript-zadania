b81961e583ea0e07fefa8ca2b7d45553df04f985
Bartłomiej Talarczyk <btalarczyk1@gmail.com>
2020-10-26 23:17:10 +0100

    1. Początki

---
+++ 1.Początki/script.js
@@ -0,0 +1,12 @@
+// zadanie 1
+console.log("Bartek");
+
+// zadanie 2
+console.log(12 + 32);
+console.log(52 / 16);
+console.log(5 * 2);
+
+// zadanie 3
+// ad1. string 'Hello World'
+// ad2. error
+// ad3. unclosed quote (error)
